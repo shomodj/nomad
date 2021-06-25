7b91b216e4bfbf9f43a972dbf95579c9fa5e2a7b
Tim Gross <tgross@hashicorp.com>
2021-06-25 08:22:03 -0400

    prevent tests from writing to disk

--- tools/nomad-generate/nomad_generate_test.go
+++ tools/nomad-generate/nomad_generate_test.go
@@ -7,7 +7,9 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestGenerate_All(t *testing.T) {
+func TestGenerate_Equals(t *testing.T) {
+	require := require.New(t)
+
 	g := &Generator{
 		typeNames:      []string{"Job"},
 		packageDir:     "../../nomad/structs",
@@ -15,8 +17,45 @@ func TestGenerate_All(t *testing.T) {
 		excludedFields: []string{"Job.Payload"},
 		typeSpecs:      map[string]*TypeSpecNode{},
 	}
-	err := run(g)
-	require.NoError(t, err)
+	pkgs, err := g.loadPackages()
+	require.NoError(err)
+
+	err = g.parsePackages(pkgs)
+	require.NoError(err)
+
+	err = g.analyze()
+	require.NoError(err)
+
+	// this is g.render without writing to disk
+	var buf bytes.Buffer
+	err = g.write(&buf, "./structs.equals.tmpl", g)
+	require.NoError(err)
+
+	formatted := g.format(buf.Bytes())
+	got := string(formatted)
+
+	require.Contains(got, "if j.ID != other.ID {")
+	require.Contains(got, "if j.Dispatched != other.Dispatched {")
+
+	// TODO: is this "&" right?
+	require.Contains(got, "if !j.Multiregion.Equals(&other.Multiregion) {")
+
+	// TODO: this is a struct, is this right?
+	require.Contains(got, "if j.Update != other.Update {")
+
+	// TODO: missing this
+	// require.Contains(got, `
+	// for i, v := range j.Datacenters {
+	// 	if v != other.Datacenters[i] {
+	// 		return false
+	// `)
+
+	// TODO: missing this
+	// require.Contains(got, `
+	// for i, v := range j.TaskGroups {
+	// 	if !v.Equals(other.TaskGroups[i]) {
+	// 		return false
+	// `)
 }
 
 func TestGenerate_Copy(t *testing.T) {
